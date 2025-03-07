accefcfcf221642e3b50acce44b381402f107cb6
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-03-07 09:46:31 +0530

    Calling the abstract methods for calculation
    Calling the abstract methods for calculation of area and perimeter of square

--- Square.java
+++ Square.java
@@ -3,4 +3,17 @@ class Square extends Shape{
     public Square(double length){
         super(length, 0, 0, 4);
     }
+
+    //Calling the methods frokm the abstract class of shape
+    public double calculateArea(){
+        return Math.pow(dim_one, 2);
+    }
+
+    public double calculatePerimeter(){
+        return 4 *(dim_one);
+    }
+
+    public double calculateVolume(){
+        return 0;
+    }    
 }
