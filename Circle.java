9ed49b8e1aa34e5e8fa001db30991d50a6c09154
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-03-07 09:44:10 +0530

    calling the abstract methods for calculation
    calling the abstract methods for calculation of area, perimeter

--- Circle.java
+++ Circle.java
@@ -3,4 +3,17 @@ class Circle extends Shape{
     public Circle(double length){
         super(length, 0, 0, 0);
     }
+
+    //Calling the methods frokm the abstract class of shape
+    public double calculateArea(){
+        return (Math.PI)*Math.pow(dim_one, 2);
+    }
+
+    public double calculatePerimeter(){
+        return 2 *Math.PI*dim_one;
+    }
+
+    public double calculateVolume(){
+        return 0;
+    }
 }
