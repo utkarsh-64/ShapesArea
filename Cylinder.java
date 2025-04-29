c88ece5c4d6a61a35b1f96656ce2ea484a8d226b
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-04-29 15:10:11 +0530

    Calling the abstract methods for calculation

--- Cylinder.java
+++ Cylinder.java
@@ -1,3 +1,19 @@
 //Cylinder.java
 class Cylinder extends Shape{
+    public Cylinder(double radius, double height){
+        super(radius, 0, height, 3);
+    }
+
+    //Calling the methods frokm the abstract class of shape
+    public double calculateArea(){
+        return 2*(Math.PI)*dim_one*(dim_one + dim_three);
+    }
+
+    public double calculatePerimeter(){
+        return 2 * (dim_one + dim_three);
+    }
+
+    public double calculateVolume(){
+        return (Math.PI)*Math.pow(dim_one, 2)*dim_three;
+    }
 }
