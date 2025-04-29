262697e7076d965f0f0e283d64e528c43570223c
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-04-29 15:13:45 +0530

    Creating Rectangle class and Calling Abstract methods

--- Rectangle.java
+++ Rectangle.java
@@ -1,1 +1,19 @@
+//Rectangle.java
+class Rectangle extends Shape{
+    public Rectangle(double length, double width){
+        super(length, width, 0, 4);
+    }
 
+    //Calling the methods frokm the abstract class of shape
+    public double calculateArea(){
+        return dim_one * dim_two;
+    }
+
+    public double calculatePerimeter(){
+        return 2 *(dim_one + dim_two);
+    }
+
+    public double calculateVolume(){
+        return 0;
+    }
+}
