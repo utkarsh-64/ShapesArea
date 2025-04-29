07a890bffc85a0990e0adc5ddae00b653966931f
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-04-29 15:11:29 +0530

    Calling the abstract methods for calculation

--- Pyramid.java
+++ Pyramid.java
@@ -1,3 +1,19 @@
 // Pyramid.java
 class Pyramid extends Shape{
+    public Pyramid(double length, double height){
+        super(length,0, height, 6);
+    }
+
+    //Calling the methods frokm the abstract class of shape
+    public double calculateArea(){
+        return (Math.sqrt(3)/4)*Math.pow(dim_one, 2);
+    }
+
+    public double calculatePerimeter(){
+        return 0;
+    }
+
+    public double calculateVolume(){
+        return (Math.sqrt(3)/4)*Math.pow(dim_one, 2)*dim_three;
+    }
 }
