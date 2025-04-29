3d27ef14e85e3c3b813d9701a6a1085e587fdb22
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-04-29 15:12:49 +0530

    Calling the abstract methods for calculation

--- Sphere.java
+++ Sphere.java
@@ -1,3 +1,19 @@
 //Sphere.java
 class Sphere extends Shape{
+    public Sphere(double radius){
+        super(radius, 0, 0, 0);
+    }
+
+    //Calling the methods frokm the abstract class of shape
+    public double calculateArea(){
+        return 4*(Math.PI)*Math.pow(dim_one, 2);
+    }
+
+    public double calculatePerimeter(){
+        return 0;
+    }
+
+    public double calculateVolume(){
+        return (4/3)*(Math.PI)*Math.pow(dim_one, 3);
+    }
 }
